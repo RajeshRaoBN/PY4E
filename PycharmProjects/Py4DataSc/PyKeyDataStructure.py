d331e45b13e700cdd35b1daedf921c5b196a26c3
Rajesh Narayana Rao <mailrbn@gmail.com>
2018-02-11 22:00:24 +1100

    Final

---
+++ PycharmProjects/Py4DataSc/PyKeyDataStructure.py
@@ -0,0 +1,6 @@
+x = [10,20,30]
+y = x
+x[1] = 42
+print(y)
+
+# Answer will be [10, 42, 30]
--- WebstormProjects/w3SchoolProject/Demo.js
+++ WebstormProjects/w3SchoolProject/Demo.js
@@ -1,3 +1,7 @@
 /**
  * Created by rbnra on 4/07/2017.
  */
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
